e191a4a6e2abcb624580d514630d42c03775f39c
Anoop Naravaram <anoop901@gmail.com>
2023-07-19 01:57:18 -0700

    redirect root to basePath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/decision-helper",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/decision-helper",
+        basePath: false,
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
